7e1359f635d4a097e83e237cf7d80f71e4d1dda2
lianet <jack.tolmak@yandex.ru>
2024-08-14 16:00:06 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,10 @@
 "use strict";
 
-import gsap from "gsap";
 
-//  init Fancybox
-if (typeof Fancybox !== "undefined" && Fancybox !== null) {
-    Fancybox.bind("[data-fancybox]", {
-        dragToClose: false,
-        closeButton: false
-    });
-}
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
-    const spinnerBtn = document.querySelector('.main__spinner-btn');
-    const spinnerImage = document.querySelector('.main__spinner-wheel');
-
-    spinnerBtn.addEventListener('click', () => {
-        spinnerBtn.style.pointerEvents = "none";
-        const turns = 2.375;
-        const duration = 3;
-
-        const totalDegrees = turns * 360;
-
-
-        spinnerImage.style.setProperty('--rotate-degrees', `${totalDegrees}deg`);
-        spinnerImage.style.animation = `spin ${duration}s ease-out forwards`;
-
-        setTimeout(() => {
-
-            Fancybox.show([{
-                src: '#congratulations',
-                type: 'inline',
-            }], {
-                on: {
-                    "done": (instance, current) => {
-                        spinnerBtn.style = "none";
-                    },
-                    "close": (instance, current) => {
-                        spinnerImage.style = ""
-
-                    }
-                }
-            });
-
-        }, duration * 1000);
-    });
-
-    const dotAnimation = gsap.timeline();
-
-
-    dotAnimation.to(".dot-even", {
-        opacity: 0,
-        scale: 1.2,
-        duration: 0.5,
-        yoyo: true,
-        repeat: -1,
-        ease: "none"
-    });
-
-
-    dotAnimation.to(".dot-odd", {
-        opacity: 0,
-        scale: 1.2,
-        duration: 0.5,
-        yoyo: true,
-        repeat: -1,
-        ease: "none"
-    });
 
 
 })
