e5cb7b600adb9d5ad8d3b5db59e6d544f906df79
lianet <jack.tolmak@yandex.ru>
2024-09-01 15:21:09 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -17,32 +17,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const spinnerBtn = document.querySelector('.main__spinner-btn');
     const spinnerImage = document.querySelector('.main__spinner-wheel');
 
+    let clickCount = 0;
+
     spinnerBtn.addEventListener('click', () => {
+        clickCount++;
 
-        const turns = 2.5;
+        let turns;
+        let modalId;
+
+        if (clickCount === 1) {
+            turns = 2.25;
+            modalId = '#sorry';
+        } else {
+            turns = 2.5;
+            modalId = '#congratulations';
+        }
+
         const duration = 3;
-
         const totalDegrees = turns * 360;
-
 
         spinnerImage.style.setProperty('--rotate-degrees', `${totalDegrees}deg`);
         spinnerImage.style.animation = `spin ${duration}s ease-out forwards`;
 
         setTimeout(() => {
-
             Fancybox.show([{
-                src: '#congratulations',
+                src: modalId,
                 type: 'inline',
+                closeButton: false
             }], {
                 on: {
                     "done": (instance, current) => {
-                        setTimeout(() => {
-                            spinnerImage.style = ""
-                        }, 1000)
+                        spinnerBtn.style = "none";
+                    },
+                    "close": (instance, current) => {
+                        spinnerImage.style = ""
                     }
                 }
             });
-
         }, duration * 1000);
     });
 
